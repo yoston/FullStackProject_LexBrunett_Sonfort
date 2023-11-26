029f9d542f386dfe7c71fa079bd444fbe3bc678e
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-11-26 22:37:57 +0000

    First branch models added

--- src/api/models.py
+++ src/api/models.py
@@ -6,6 +6,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    phone_number = db.Column(db.String(80), unique=False, nullable=False)
+    Addres = db.Column(db.String(500), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -16,4 +18,30 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+    
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    description = db.Column(db.String(500), unique=False, nullable=False)
+    Category = db.Column(db.String(80), unique=False, nullable=False)
+    price = db.Column(db.Integer, unique=False, nullable=False)
+    amount = db.Column(db.Integer, unique=False, nullable=False)
+    img = db.Column(db.String(250), unique=False, nullable=True)
+    idu = db.Column(db.String(250), unique=False, nullable=True)
+
+
+    def __repr__(self):
+        return f'<User {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "Category": self.Category,
+            "price": self.price,
+            "amount": self.amount,
+            "img": self.img,
+            "idu": self.idu
         }
