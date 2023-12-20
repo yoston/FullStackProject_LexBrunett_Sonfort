e28d44d95c306fe8c1f258359f9481b1e516b8af
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-12-20 04:13:58 +0000

    corrections

--- src/api/models.py
+++ src/api/models.py
@@ -2,25 +2,112 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
-class Orders(db.Model):
-    __tablename__ = 'orders'
+class User (db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.Boolean(), unique=True, nullable=False)
-    payment = db.Column(db.Boolean(), unique=False, nullable=False)
-    # Establece la relación uno a muchos con productos
-    products = db.relationship('Product', backref='Trademark', lazy=True)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(20), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=True)
 
     def __repr__(self):
+        return f'<User {self.email}>'
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+        }
+
+class Product (db.Model):
+    __tablename__ = 'product'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    description = db.Column(db.String(500), unique=False, nullable=False)
+    price = db.Column(db.Integer, unique=False, nullable=False)
+    amount = db.Column(db.Integer, unique=False, nullable=False)
+    url_img = db.Column(db.String(250), unique=False, nullable=True)
+    idu_img = db.Column(db.String(250), unique=False, nullable=True)
+    id_category = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    category = db.relationship('Category')
+
+    def __repr__(self):
+        return f'<Product {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
             "name": self.name,
-            "url_img": self.url_img,
-            "idu_img": self.idu_img
             "description": self.description,
             "price": self.price,
             "amount": self.amount,
+            "url_img": self.url_img,
+            "idu_img": self.idu_img,
+            "id_category": self.id_category
+        }
+class Category (db.Model):
+    __tablename__ = 'category'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    url_img = db.Column(db.String(320), unique=False, nullable=False)
+    idu_img = db.Column(db.String(320), unique=False, nullable=False)
+    
+    def __repr__(self):
+        return f'<Category {self.name}>'
+      
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "url_img": self.url_img,
+            "idu_img": self.idu_img
+        }
 
+class Order (db.Model):
+    __tablename__ = 'order'
+    id = db.Column(db.String , primary_key=True)
+    state = db.Column(db.String(80), unique=False, nullable=False)
+    day_Date = db.Column(db.String(20), unique=False, nullable=False)
+    month_Date = db.Column(db.String(20), unique=False, nullable=False)
+    year_Date = db.Column(db.String(20), unique=False, nullable=False)
+    value = db.Column(db.Integer, unique=False, nullable=True)
+    id_Restaurant = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+    restaurant = db.relationship('Restaurant')
+    id_Sucursale = db.Column(db.Integer, db.ForeignKey('sucursale.id'), nullable=False)
+    sucursale = db.relationship('Sucursale')
+
+    def __repr__(self):
+        return f'<Orden {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "state": self.state,
+            "day_Date": self.day_Date,
+            "month_Date": self.month_Date,
+            "year_Date": self.year_Date,
+            "value": self.value,
+            "id_Restaurant": self.id_Restaurant,
+            "id_Sucursale": self.id_Sucursale
+        }
+
+class Cart(db.Model):
+    __tablename__ = 'cart'
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Integer, unique=False, nullable=False)
+    id_Product = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    product = db.relationship('Product')
+    id_Restaurant = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+    restaurant = db.relationship('Restaurant')
+    id_Order = db.Column(db.String, db.ForeignKey('order.id'), nullable=True)
+    order = db.relationship('Order')
+
+    def __repr__(self):
+        return f'<Cart {self.id}>'
+    def serialize(self):
+        return {
+            "id": self.id,
+            "amount": self.amount,
+            "id_Product": self.id_Product,
+            "id_Restaurant": self.id_Restaurant,
+            "id_Order": self.id_Order
+        }
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,106 +1,5 @@
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Admin, Orders
-from api.utils import generate_sitemap
-
-api = Blueprint('api', __name__)
-
-@api.route('/admin', methods=['GET'])
-def get_admins():
-
-    all_admin = Admin.query.all()
-    admin_serialize = [Admin.serialize() for Admin in all_admin]
-
-    return jsonify(admin_serialize), 200
-
-@api.route('/admin', methods=['POST'])
-def post_admins():
-
-    body = request.json
-    new_admin = Orders(id=body['id'],name=body['name'],description=body['description'],price=body['price'],amount=body['amount'])
-    db.session.add(new_admin)
-    db.session.commit()
-    
-    return jsonify({"message": "Admin creado con éxito"}), 200
-
-@api.route('/admin/<id>', methods=['PUT'])
-def put_admin(id):
-    admin = Admin.query.get(id)
-    body = request.json
-
-    if not admin:
-        return jsonify({"message": "Admin no encontrado"}), 404
-    
-    admin.name = body['name']
-    admin.description = body['description']
-    admin.category = body['category']
-    admin.products = body['products']
-
-    db.session.commit()
-    
-    return jsonify({"message": "Admin modificada con éxito"}), 200
-
-@api.route('/admin/<id>', methods=['DELETE'])
-def delete_admin(id):
-
-    admin = Admin.query.get(id)
-
-    if not admin:
-        return jsonify({"message": "Admin no encontrado"}), 404
-
-    db.session.delete(admin)
-    db.session.commit()
-    
-    return jsonify({"message": "Admin eliminada con éxito"}), 200
-
-@api.route('/Orders', methods=['GET'])
-def get_Orders():
-
-    all_Orders = Orders.query.all()
-    Orders_serialize = [Orders.serialize() for Orders in all_Orders]
-
-    return jsonify(Orders_serialize), 200
-
-@api.route('/Orders', methods=['POST'])
-def post_Orders():
-
-    body = request.json
-    new_Orders = Orders(id=body['id'],status=body['status'],payment=body['payment'],products=body['products'])
-    db.session.add(new_admin)
-    db.session.commit()
-    
-
-    return jsonify({"message": "Order creado con éxito"}), 200
-
-@api.route('/Orders/<id>', methods=['PUT'])
-def put_admin(id):
-    Orders = Orders.query.get(id)
-    body = request.json
-
-    if not Orders:
-        return jsonify({"message": "Orders no encontrado"}), 404
-    
-    Orders.status = body['status']
-    Orders.payment = body['payment']
-    Orders.products = body['products']
-
-    db.session.commit()
-    
-    return jsonify({"message": "Orders modificada con éxito"}), 200
-
-@api.route('/Orders/<id>', methods=['DELETE'])
-def delete_Orders(id):
-
-    Orders = Orders.query.get(id)
-
-    if not Orders:
-        return jsonify({"message": "Orders no encontrado"}), 404
-
-    db.session.delete(Orders)
-    db.session.commit()
-    
-    return jsonify({"message": "Orders eliminada con éxito"}), 200
-
-from api.models import db, User, Product, Categories, Cart,  Orders
+from api.models import db, User, Product, Category, Cart, Order
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
@@ -108,6 +7,146 @@ from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
 
 api = Blueprint('api', __name__)
+
+@api.route('/user', methods=['GET'])
+def get_user():
+    all_user = User.query.all()
+    user_serialize = [user.serialize() for user in all_user]
+    return jsonify(user_serialize), 200
+
+@api.route("/user", methods=["POST"])
+def post_user():
+    body = request.json
+    user = User.query.filter_by(email = body['email']).first()
+    
+    if user:
+        return jsonify({"msg": "Usuario ya existe"}), 401
+    
+    new_user = User(
+        email=body['email'],
+        password=body["password"]
+    )
+
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"msg" : "Usuario creado"}) , 200
+
+@api.route('/user/<int:id>', methods=['PUT'])
+def put_user(id):
+    user = User.query.get(id)
+
+    if not user:
+        return jsonify({"message": "Usuario no encontrado"}), 404
+    
+    body = request.json
+    user.email = body['email']
+    user.password = body['password']
+
+    db.session.commit()
+
+    return jsonify({"message": "Usuario modificado con éxito"}), 200
+
+@api.route('/user/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    user = User.query.get(id)
+
+    if not user:
+        return jsonify({"message": "Usuario no encontrado"}), 404
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"message": "Producto eliminado con éxito"}), 200
+
+@api.route("/login_user", methods=["POST"])
+def post_login_user():
+    name = request.json.get("name", None)
+    password = request.json.get("password", None)
+    
+    user = Restaurant.query.filter_by(name=name, password=password).first()
+    
+    if user is None:
+        return jsonify({"msg": "Bad username or password"}), 401
+ 
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id , "user":"restaurant", "name": user.name}) , 200 
+
+@api.route("/login_admin", methods=["POST"])
+def post_login_admin():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    
+    user = User.query.filter_by(email=email, password=password).first()
+
+    if user is None:
+        return jsonify({"msg": "Bad username or password"}), 401
+ 
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id , "user":"admin"}) , 200
+
+@api.route('/products', methods=['GET'])
+def get_products():
+    all_products = Product.query.all()
+    products_serialize = [product.serialize() for product in all_products]
+    product_with_product_info = []
+
+    for item in products_serialize:
+        category_id = item["id_category"]
+        category = Category.query.get(category_id)
+        
+        if category:
+            item['category_info'] = category.serialize()
+            product_with_product_info.append(item)
+
+    return jsonify(products_serialize), 200
+
+@api.route('/products', methods=['POST'])
+def post_product():
+    body = request.json
+    
+    new_product = Product(
+        name=body['name'],
+        description=body['description'],
+        price=body['price'],
+        amount=body['amount'],
+        url_img=body['url_img'],
+        idu_img=body['idu_img'],
+        id_category= body['id_category']
+    )
+
+    db.session.add(new_product)
+    db.session.commit()
+
+    return jsonify({"message": "Producto creado con éxito"}), 200
+    
+@api.route('/products/<int:id>', methods=['PUT'])
+def put_product(id):
+    product = Product.query.get(id)
+
+    if not product:
+        return jsonify({"message": "Producto no encontrado"}), 404
+    
+    body = request.json
+    product.name = body['name']
+    product.description = body['description']
+    product.price = body['price']
+    product.amount = body['amount']
+    product.url_img = body['url']
+    product.idu_img = body['idu']
+    product.id_category = body['id_category']
+    db.session.commit()
+
+    return jsonify({"message": "Producto modificado con éxito"}), 200
+
+@api.route('/products/<int:id>', methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get(id)
+
+    if not product:
+        return jsonify({"message": "Producto no encontrado"}), 404
+    
+    db.session.delete(product)
+    db.session.commit()
+
+    return jsonify({"message": "Producto eliminado con éxito"}), 200
 
 @api.route('/category', methods=['GET'])
 def get_categories():
@@ -157,3 +196,200 @@ def delete_categories(id):
 
     return jsonify({"message": "Categoría eliminada con éxito"}), 200
 
+@api.route('/cart', methods=['GET'])
+@jwt_required()
+def get_carts():
+    id = get_jwt_identity()
+
+    all_items = Cart.query.filter_by(id_Restaurant=id, id_Order=None).all()
+    items_serialize = [item.serialize() for item in all_items]
+    cart_with_product_info = []
+
+    for item in items_serialize:
+        product_id = item["id_Product"]
+        product = Product.query.get(product_id)
+        if product:
+            item['product_info'] = product.serialize()
+            cart_with_product_info.append(item)
+    print(cart_with_product_info)
+    return jsonify(cart_with_product_info), 200
+
+@api.route('/cart/<int:id>', methods=['PUT'])
+def put_cart(id):
+    cart = Cart.query.get(id)
+
+    if not cart:
+        return jsonify({"message": "Carrito no encontrado"}), 404
+    
+    body = request.json
+
+    cart.amount = body['amount']
+    cart.id_Producto = body['id_Product']
+    cart.id_Restaurant = body['id_Restaurant']
+    cart.id_Order = body['id_Order']
+
+    db.session.commit()
+
+    return jsonify({"message": "Carrito modificado con éxito"}), 200
+
+@api.route('/cart_add_idOrder/<int:id>', methods=['PUT'])
+def add_order_cart(id):
+    cart = Cart.query.get(id)
+
+    if not cart:
+        return jsonify({"message": "Carrito no encontrado"}), 404
+    
+    body = request.json
+
+    cart.amount = body['amount']
+    cart.id_Producto = body['id_Product']
+    cart.id_Restaurant = body['id_Restaurant']
+    cart.id_Order = body['id_Order']
+
+    db.session.commit()
+
+    return jsonify({"message": "orden annadida con éxito"}), 200
+
+@api.route('/cart', methods=['POST'])
+def post_cart():
+    body = request.json
+
+    existente = Cart.query.filter_by(id_Product = body['id_Product'], id_Restaurant = body['id_Restaurant'], id_Order = None).first()
+
+    if (existente):
+        existente.amount = existente.amount + 1,
+        existente.id_Product=body['id_Product'],
+        existente.id_Restaurant=body['id_Restaurant'],
+        existente.id_Order = body['id_Order'],
+
+        db.session.commit()
+    else :
+        new_cart = Cart(
+            amount=body['amount'],
+            id_Product=body['id_Product'],
+            id_Restaurant=body['id_Restaurant'],
+            id_Order = body['id_Order']
+        )
+
+        db.session.add(new_cart)
+        db.session.commit()
+
+    return jsonify({"message": "Carrito creado con éxito"}), 200
+
+@api.route('/cart/<int:id>', methods=['DELETE'])
+def delete_cart(id):
+    cart = Cart.query.get(id)
+
+    if not cart:
+        return jsonify({"message": "Carrito no encontrado"}), 404
+    
+    db.session.delete(cart)
+    db.session.commit()
+
+    return jsonify({"message": "Carrito eliminado con éxito"}), 200
+
+@api.route('/order', methods=['GET'])
+@jwt_required()
+def get_order(): 
+    restaurant_id = get_jwt_identity()
+    
+    all_order = Order.query.filter_by( id_Restaurant = restaurant_id ).all()
+    order_seriallize = [item.serialize() for item in all_order]
+    order_with_info = []
+
+    for item in order_seriallize:
+        restaurant_id = item["id_Restaurant"]
+        sucursale_id = item["id_Sucursale"]
+        restaurant = Restaurant.query.get(restaurant_id)
+        sucursale = Sucursale.query.get(sucursale_id)
+
+        order_item = item.copy()
+
+        order_item['restaurant_info'] = restaurant.serialize()
+        order_item['sucursale_info'] = sucursale.serialize()
+        order_with_info.append(order_item)
+
+    return jsonify(order_with_info), 200
+
+@api.route('/all_order', methods=['GET'])
+@jwt_required()
+def get_all_order():
+    all_order = Order.query.all()
+    order_with_info = []
+
+    for item in all_order:
+        restaurant_id = item.id_Restaurant
+        sucursale_id = item.id_Sucursale
+        restaurant = Restaurant.query.get(restaurant_id)
+        sucursale = Sucursale.query.get(sucursale_id)
+        carts = Cart.query.filter_by(id_Order=item.id)
+        cart_with_product_info = []
+
+        for cart in carts:
+            product = Product.query.get(cart.id_Product)
+            cart_info = {
+                'product_info': product.serialize(),
+            }
+            cart_with_product_info.append(cart_info)
+
+        order_item = item.serialize()
+
+        order_item['restaurant_info'] = restaurant.serialize()
+        order_item['sucursale_info'] = sucursale.serialize()
+        order_item['products'] = cart_with_product_info
+        order_with_info.append(order_item)
+
+    return jsonify(order_with_info), 200
+
+
+@api.route('/order/<id>', methods=['PUT'])
+def put_order(id):
+    order = Order.query.get(id)
+    body = request.json
+
+    if not order:
+        return jsonify({"message": "Orden no encontrada"}), 404
+    
+    order.state = body['state']
+    order.day_Date = body['day_Date']
+    order.month_Date = body["month_Date"]
+    order.year_Date = body["year_Date"]
+    order.value = body["value"]
+    order.id_Restaurant = body['id_Restaurant']
+    order.id_Sucursale = body['id_Sucursale']
+    
+    db.session.commit()
+
+    return jsonify({"message": "Orden modificada con éxito"}), 200
+
+@api.route('/order', methods=['POST'])
+def post_order():
+    body = request.json
+    new_order = Order(
+        id=body["id"],
+        state= "Creada",
+        day_Date=body["day_Date"],
+        month_Date=body["month_Date"],
+        value=body["value"],
+        year_Date=body["year_Date"],
+        id_Restaurant=body["id_Restaurant"],
+        id_Sucursale=body["id_Sucursale"],
+    )
+
+    db.session.add(new_order)
+    db.session.commit()
+
+    return jsonify({"message": "Orden creada con éxito"}), 200
+
+@api.route('/order/<id>', methods=['DELETE'])
+def delete_order(id):
+
+    order = Order.query.get(id)
+
+    if not order:
+        return jsonify({"message": "Orden no encontrada"}), 404
+
+    db.session.delete(order)
+    db.session.commit()
+    
+    return jsonify({"message": "Orden eliminada con éxito"}), 200
